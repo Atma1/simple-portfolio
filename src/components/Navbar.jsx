da62df593cc80b514de425f89fce5457ce270c3b
Atma1 <atmasusanto08@gmail.com>
2024-08-11 11:29:23 +0800

    removed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import atmaLogo from '../assets/atma.png';
 export default function Navbar() {
     return (
         <nav id="navbar">
-            <img src={atmaLogo} id='atma-logo' alt="Logo atma"/>
             <ul>
                 <li><a href="#welcome-section">About Me</a></li>
                 <li><a href="#projects">Past Projects</a></li>
