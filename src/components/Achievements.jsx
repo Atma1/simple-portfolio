cbacfa158b61611bfc55ad6502144c48df81f48b
Atma1 <atmasusanto08@gmail.com>
2024-08-12 16:49:52 +0800

    Init

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,16 +1,17 @@
 import { Box, CardContent, CardMedia, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
+import fitLogo from '../assets/fit.png';
 
 export default function Achievements() {
     return (
         <div id='achievements'>
             <h2 className="title">Achievements</h2>
-            <Box>
-                <Card sx={{display: 'flex', marginTop: '1em'}}>
+            <Box sx={{p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Card sx={{display: 'flex', marginTop: '1em', width: 500}}>
                     <CardMedia 
                         component='img'
                         image='https://pusatprestasinasional.kemdikbud.go.id/uploads/event/mln97DL3c4QlAyKLnP7yxJcQmndDl9xSm5wj6qbF.png'
-                        sx={{width: 151}}
+                        sx={{width: 151, padding: 1}}
                     />
                     <CardContent sx={{display: 'flex', alignItems: 'center'}}>
                         <Typography>
@@ -18,15 +19,15 @@ export default function Achievements() {
                         </Typography>
                     </CardContent>
                 </Card>
-                <Card sx={{display: 'flex', marginTop: '1em'}}>
+                <Card sx={{display: 'flex', marginTop: '1em',  width: 500}}>
                     <CardMedia 
                         component='img'
-                        image='https://pusatprestasinasional.kemdikbud.go.id/uploads/event/mln97DL3c4QlAyKLnP7yxJcQmndDl9xSm5wj6qbF.png'
-                        sx={{width: 151}}
+                        image={fitLogo}
+                        sx={{width: 151, padding: .5}}
                     />
                     <CardContent sx={{display: 'flex', alignItems: 'center'}}>
                         <Typography>
-                            2nd Place KKCTBN 2023, ATSV Category
+                            3rd Place FIT Competition 2024, Web Dev Category
                         </Typography>
                     </CardContent>
                 </Card>
